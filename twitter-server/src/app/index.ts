cd4230eb120c9c814fca4f9a1ec7bfda21684b48
Pradum <satendarpradum@gmail.com>
2024-11-22 15:20:34 +0530

    Setup database and some ui

---
+++ twitter-server/src/app/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import bodyParser from 'body-parser';
+
+export async function initServer(){
+ const app=express();
+
+ app.use(bodyParser.json());
+
+ const graphqlServer=new ApolloServer({
+    typeDefs:`
+    type Query{
+    sayHello:string
+    sayHelloToMe(name:String!):String
+    }
+    
+    `,
+    resolvers:{
+        Query:{
+            sayHello:()=>"Hey from GraphQl Server",
+            sayHelloToMe:(parent:any,{name}:{name:String})=>null,
+        },
+        
+    },
+ })
+
+ await graphqlServer.start();
+ app.use('/graphql',expressMiddleware(graphqlServer));
+ return app
+}
